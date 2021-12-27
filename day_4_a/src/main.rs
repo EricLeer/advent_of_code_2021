a8de254413d09a10f4cdda79f872d7d3e7d15496
EricLeer <eric.leerssen@gmail.com>
2021-12-27 22:14:59 +0100

    Add start of day 4 puzzle

--- day_4_a/src/main.rs
+++ day_4_a/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "/Users/ericleer/projects/rust_projects/advent_of_code_2021/day_4_a/data/test_input.txt";
+
+    let puzzle_input = fs::read_to_string(filename).unwrap();
+
+    let mut puzzle_input_iter = puzzle_input.lines();
+
+    let bingo_numbers = puzzle_input_iter.next().unwrap();
+    
+    println!("{}", bingo_numbers);
 }

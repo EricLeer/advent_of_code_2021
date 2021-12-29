996198b209dd26a78e42e8ea75a0a4829d38b5d8
EricLeer <eric.leerssen@gmail.com>
2021-12-29 16:59:50 +0100

    Add day_4_a solution

--- day_4_a/src/main.rs
+++ day_4_a/src/main.rs
@@ -1,13 +1,130 @@
 use std::fs;
 
-fn main() {
-    let filename = "/Users/ericleer/projects/rust_projects/advent_of_code_2021/day_4_a/data/test_input.txt";
+struct BingoNumber {
+    number: u32,
+    crossed: bool,
+}
 
-    let puzzle_input = fs::read_to_string(filename).unwrap();
+impl BingoNumber {
+    fn new(number: u32) -> BingoNumber {
+        BingoNumber { number: number, crossed: false }
+    }
+}
 
-    let mut puzzle_input_iter = puzzle_input.lines();
+struct BingoBoard {
+    board: Vec<BingoNumber>,
+}
 
-    let bingo_numbers = puzzle_input_iter.next().unwrap();
+impl BingoBoard {
+    fn print_board(&self) -> () {
+        for number in self.board.iter(){
+            print!("{} ", number.number);
+        }
+    }
+
+    fn cross_number(&mut self, number: u32) -> () {
+        for bingo_number in self.board.iter_mut() {
+            if bingo_number.number == number {
+                bingo_number.crossed = true;
+            }
+        }
+    }
+
+    fn index_board(&self, row: usize, col: usize) -> &BingoNumber {
+        &self.board[(row*5)+col]
+    }
+
+    fn check_win(&self) -> bool {
+        let mut all_crossed = true;
+
+        // Check rows
+        for row in 0..5 {
+            'inner_row: for col in 0..5 {
+                let number = self.index_board(row, col);
+                if number.crossed {
+                    continue
+                } else {
+                    all_crossed = false;
+                    break 'inner_row
+                }
+            }
+            if all_crossed {
+                return true
+            } else {
+                all_crossed = true;
+            }
+        }
+
+        for col in 0..5 {
+            'inner_col: for row in 0..5 {
+                let number = self.index_board(row, col);
+                if number.crossed {
+                    continue
+                } else {
+                    all_crossed = false;
+                    break 'inner_col
+                }
+            }
+            if all_crossed {
+                return true
+            } else {
+                all_crossed = true;
+            }
+        }
+        return false
+    }
+
+    fn score(&self) -> u32 {
+        let mut result = 0;
+        for number in self.board.iter() {
+            if !number.crossed {
+                result += number.number
+            }
+        }
+        result
+    }
+}
+
+fn parse_board(bingo_numbers: &str) -> BingoBoard {
+    let mut bingo_board = BingoBoard{ board: vec![]};
     
-    println!("{}", bingo_numbers);
+    for line in bingo_numbers.lines() {
+        for number in line.split_whitespace() {
+            let bingo_number = BingoNumber::new(number.parse().unwrap());
+            bingo_board.board.push(bingo_number);
+        }
+    }
+    bingo_board
+}
+
+fn main() {
+    let filename = "/Users/ericleer/projects/rust_projects/advent_of_code_2021/day_4_a/data/puzzle_input.txt";
+    let puzzle_input = fs::read_to_string(filename).unwrap();
+    let mut win = None;
+
+    let puzzle_input: Vec<&str> = puzzle_input
+        .trim()
+        .split("\n\n")
+        .collect();
+    let numbers = &puzzle_input[0];
+    let boards = &puzzle_input[1..];
+
+    let mut boards: Vec<BingoBoard> = boards.iter().map(|x| parse_board(x)).collect();
+    for number in numbers.split(",") {
+        let number = number.parse().unwrap();
+        for board in boards.iter_mut() {
+            board.cross_number(number);
+
+            if board.check_win() {
+                // board.print_board();
+                win = Some(board.score() * number);
+            }
+        }
+        if win.is_some() {
+            break
+        }
+
+    }
+
+    println!("{}", win.unwrap());
 }
